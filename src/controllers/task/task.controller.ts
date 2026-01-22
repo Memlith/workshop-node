2fe4df09b899c07d8f34af2b6b0c368fb63ed778
Memlith <iamcaiosimonassi@gmail.com>
2026-01-22 16:03:51 -0300

    Enhance task management with user authentication and update task entity
    - Added user authentication checks in task controller methods to ensure only authenticated users can access their tasks.
    - Updated task entity to include a userId field and establish a relationship with the User entity.
    - Refactored task controller methods to handle user-specific task operations, including create, read, update, and delete functionalities.

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -4,63 +4,86 @@ import Task from '../../models/task.entity'
 export default class TaskController {
     static async store(req: Request, res: Response) {
         const { title, completed } = req.body
+        const { userId } = req.headers
+
+        if (!userId) return res.status(401).json({ error: 'User not authenticated' })
 
         if (!title) {
-            return res.status(400).json({ error: 'Title required' })
+            return res.status(400).json({ error: 'Title is required' })
         }
         const task = new Task()
         task.title = title
         task.completed = completed || false
+        task.userId = Number(userId)
         await task.save()
 
         return res.status(201).json(task)
     }
 
     static async index(req: Request, res: Response) {
-        const tasks = await Task.find()
+        const { userId } = req.headers
+
+        if (!userId) return res.status(401).json({ error: 'User not authenticated' })
+
+        const tasks = await Task.find({ where: { userId: Number(userId) } })
         return res.json(tasks)
     }
 
     static async show(req: Request, res: Response) {
         const { id } = req.params
+        const { userId } = req.headers
+
 
         if (!id || isNaN(Number(id))) {
             return res.status(400).json({ error: 'ID is required' })
         }
-        const task = await Task.findOneBy({ id: Number(id) })
+
+        if (!userId) return res.status(401).json({ error: 'User not authenticated' })
+
+        const task = await Task.findOneBy({ id: Number(id), userId: Number(userId) })
         return res.json(task)
-    }
-
-    static async delete(req: Request, res: Response) {
-        const { id } = req.params
-
-        if (!id || isNaN(Number(id))) {
-            return res.status(400).json({ error: 'ID is required' })
-        }
-
-        const task = await Task.findOneBy({ id: Number(id) })
-        if (!task) {
-            return res.status(404).json({ error: 'Task not found' })
-        }
-
-        await task.remove
-        return res.status(204).json() // vamos retornar 204 pois nao temos conteudo para retornar
     }
 
     static async update(req: Request, res: Response) {
         const { id } = req.params
         const { title, completed } = req.body
+        const { userId } = req.headers
+
         if (!id || isNaN(Number(id))) {
             return res.status(400).json({ error: 'ID is required' })
         }
 
-        const task = await Task.findOneBy({ id: Number(id) })
+        if (!userId) return res.status(401).json({ error: 'User not authenticated' })
+
+        const task = await Task.findOneBy({ id: Number(id), userId: Number(userId) })
         if (!task) {
             return res.status(404).json({ error: 'Task not found' })
         }
 
-        task.title = title || task.title
+        task.title = title ?? task.title
         task.completed = (completed === undefined) ? task.completed : completed
+        await task.save()
+
+        return res.json(task)
+    }
+
+    static async delete(req: Request, res: Response) {
+        const { id } = req.params
+        const { userId } = req.headers
+
+        if (!id || isNaN(Number(id))) {
+            return res.status(400).json({ error: 'ID is required' })
+        }
+
+        if (!userId) return res.status(401).json({ error: 'User not authenticated' })
+
+        const task = await Task.findOneBy({ id: Number(id), userId: Number(userId) })
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' })
+        }
+
+        await task.remove()
+        return res.status(204).json() // vamos retornar 204 pois nao temos conteudo para retornar
     }
 }
 
--- src/models/user.entity.ts
+++ src/models/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
 import Token from './token.entity'
+import Task from './task.entity'
 
 @Entity()
 export default class User extends BaseEntity {
@@ -14,4 +15,5 @@ export default class User extends BaseEntity {
     password!: string
 
     @OneToMany(() => Token, token => token.user) tokens!: Token[]
+    @OneToMany(() => Task, task => task.user) tasks!: Task[]
 }
